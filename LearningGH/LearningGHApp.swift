fff7fe4766c684365cac7df399d0e0c8e67b7c90
rahul <iamrahulparekh@gmail.com>
2021-03-28 15:47:26 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Rahul Parekh on 2021-03-28.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
